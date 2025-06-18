d99329138cdc4c17b754cb849c3463b5da8fda9a
Aleksander Kałuski <kaluskialeksander@gmail.com>
2025-06-18 11:18:36 +0200

    +All_trials_table class

--- source/table.py
+++ source/table.py
@@ -46,7 +46,7 @@ class Table():
         if tabulate_view:
             print(tabulate(self.df, headers='keys', tablefmt='github', showindex=False))
         else:
-            display(self.df)  # ładna tabela HTML w Jupyterze
+            display(self.df)
 
     def plot_split_accuracy(self):
         """
@@ -105,33 +105,56 @@ class Table():
         plt.grid(True)
 
 
-class Time_complexity():
+class All_trials_table():
     """
     Class for keeping data about time complexity.
     """
     def __init__(self):
-        col_names = ['name', 'epochs', 'time']
+        col_names = ['loss', 'accuracy', 'params_epochs', 'params_learning_rate', 'params_units', 'time', 'trial_time', 'name']
         self.df = pd.DataFrame(columns=col_names)
 
+
     def add_record(self,
-                   name,
-                   epoch=0,
-                   lr=0.0,
-                   units=0,
-                   avg_time=0.0,
-                   two_layers=0,
-                   batch_size=0,
-                   split_accuracy=None,
-                   vocab_accuracy=None):
+                   df):
 
-        print(f"Adding a new record with params: name: {name}, epoch: {epoch}, lr: {lr}, units: {units}, average time: {avg_time}")
-
-        if split_accuracy is None:
-            split_accuracy = []
-        if vocab_accuracy is None:
-            vocab_accuracy = []
+        print(f"Adding a new record with params.")
+        self.df =pd.concat([self.df, df], ignore_index=True)
 
 
-        self.df.loc[len(self.df)] = [name, epoch, lr, units, avg_time, split_accuracy, vocab_accuracy, two_layers, batch_size]
+    def show(self, tabulate_view=True):
+        """
+        Print the contents of the table.
+        :param tabulate_view: view for normal .py file (Should be False for notebook)
+        """
+        print("\nTable info:")
+        self.df.info()
+
+        print("\nRecords preview:")
+        if tabulate_view:
+            print(tabulate(self.df, headers='keys', tablefmt='github', showindex=False))
+        else:
+            display(self.df)
+
+    import matplotlib.pyplot as plt
+
+    def plot_time_complexity(self):
+        plt.figure(figsize=(10, 5))
+        p = self.df.copy()
+
+        p.sort_values(by=['trial_time', 'params_epochs'], ascending=False, inplace=True)
+        for name in self.df['name'].unique():
+            subset = self.df[self.df['name'] == name]
+            plt.scatter(subset['trial_time'], subset['params_epochs'], label=name)
+
+
+        plt.title('Time Complexity by Model')
+        plt.xlabel('Trial Time')
+        plt.ylabel('Number of Epochs')
+        plt.legend(title='Model Name')
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show()
+
+
 
 
